ebf415735f6966ec5c29140db374c1e267648c34
SioneUhlig <sioneuhlig@gmail.com>
2025-11-19 19:40:54 -0600

    week 4 1.1

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,20 +4,6 @@ const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
 
-/* ****************************************
-*  Deliver login view
-* *************************************** */
-async function buildLogin(req, res, next) {
-  let nav = await utilities.getNav()
-  res.render("account/login", {
-    title: "Login",
-    nav,
-  })
-}
-
-module.exports = { buildLogin }
-
-
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
