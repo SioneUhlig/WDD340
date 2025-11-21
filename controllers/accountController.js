b546a7b0e3a3d79ac7d5c372dfb863568e8aa3dd
SioneUhlig <sioneuhlig@gmail.com>
2025-11-21 17:52:31 -0600

    week 4 1.3

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
 
 const accountCont = {}
 
@@ -11,6 +12,7 @@ accountCont.buildLogin = async function (req, res, next) {
   res.render("account/login", {
     title: "Login",
     nav,
+    errors: null,
   })
 }
 
@@ -33,11 +35,25 @@ accountCont.registerAccount = async function (req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -48,14 +64,33 @@ accountCont.registerAccount = async function (req, res) {
     res.status(201).render("account/login", {
       title: "Login",
       nav,
+      errors: null,
     })
   } else {
     req.flash("notice", "Sorry, the registration failed.")
     res.status(501).render("account/register", {
       title: "Registration",
       nav,
+      errors: null,
     })
   }
+}
+
+/* ****************************************
+*  Process login request
+* *************************************** */
+accountCont.processLogin = async function (req, res) {
+  let nav = await utilities.getNav()
+  const { account_email, account_password } = req.body
+  
+  // TODO: Implement actual login logic here
+  // For now, just send a success message
+  req.flash("notice", "Login functionality will be implemented in a future update.")
+  res.status(200).render("account/login", {
+    title: "Login",
+    nav,
+    errors: null,
+  })
 }
 
 module.exports = accountCont
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -11,6 +12,19 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Process the registration data
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
+
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  utilities.handleErrors(accountController.processLogin)
+)
 
 module.exports = router;
